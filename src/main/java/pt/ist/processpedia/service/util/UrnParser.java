8da2d6be0e7c969a7ff37bab6e1784399410550e
David Martinho <davidmartinho@gmail.com>
2011-04-16 15:03:21 +0100

    Added some copyright headers and modified the User Test.

--- src/main/java/pt/ist/processpedia/service/util/UrnParser.java
+++ src/main/java/pt/ist/processpedia/service/util/UrnParser.java
@@ -1,9 +1,35 @@
+/**
+ * Processpedia
+ * Copyright (C) 2011 ESW Software Engineering Group
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+
 package pt.ist.processpedia.service.util;
 
 import pt.ist.processpedia.domain.DomainObject;
 
+/**
+ * This class provides a static function which maps a Universal Resource Name (URN) to its correspondent Domain Object.
+ */
 public class UrnParser {
-  
+
+  /**
+   * Obtains the domain object identified by the provided Universal Resource Name (URN).
+   * @param urn The URN of the resource representing the domain object.
+   * @return The respective domain object.
+   */
   public static DomainObject parseUrn(String urn) {
     return null;
   }
--- src/test/java/pt/ist/processpedia/test/UserTest.java
+++ src/test/java/pt/ist/processpedia/test/UserTest.java
@@ -1,12 +1,10 @@
 package pt.ist.processpedia.test;
 
-import java.io.File;
-
 import junit.framework.TestCase;
 
 import pt.ist.fenixframework.Config;
 import pt.ist.fenixframework.FenixFramework;
-import jvstm.Transaction;
+import pt.ist.fenixframework.pstm.Transaction;
 
 import pt.ist.processpedia.domain.Processpedia;
 import pt.ist.processpedia.domain.User;
@@ -14,35 +12,30 @@ import pt.ist.processpedia.domain.User;
 import pt.ist.processpedia.domain.exception.EmailAlreadyRegisteredDomainException;
 
 public class UserTest extends TestCase {
-  
-  private final static String testDbDirectory = "/tmp/db";
-  
+
   public void setUp() {
-    deleteDir(new File(testDbDirectory));
     if(FenixFramework.getConfig()==null) {
       FenixFramework.initialize(new Config() {{
-        dbAlias=testDbDirectory;
-        domainModelPath="src/main/dml/processpedia.dml";
+        dbAlias="test-db";
+        domainModelPath="/processpedia.dml";
         repositoryType=RepositoryType.BERKELEYDB;
         rootClass=Processpedia.class;
       }});
     }
+    Transaction.begin();
   }
   
   protected void tearDown() {
-    deleteDir(new File(testDbDirectory));
-  }
-  
-  public void testUserNormalCreation() {
-    Transaction.begin();
-    Processpedia processpedia = FenixFramework.getRoot();
-    User d1 = processpedia.createNewUser("David Martinho", "davidmartinho@gmail.com", "asdf43rf3223rd2323ef2");
-    assertEquals(d1,processpedia.getUserByEmail("davidmartinho@gmail.com"));
     Transaction.abort();
   }
   
+  public void testUserNormalCreation() {
+    Processpedia processpedia = FenixFramework.getRoot();
+    User d1 = processpedia.createNewUser("David Martinho", "davidmartinho@gmail.com", "asdf43rf3223rd2323ef2");
+    assertEquals(d1,processpedia.getUserByEmail("davidmartinho@gmail.com"));
+  }
+  
   public void testUserCreationWithAlreadyRegisteredEmail() {
-    Transaction.begin();
     Processpedia processpedia = (Processpedia)FenixFramework.getRoot();
     Boolean exceptionThrown = false;
     try {
@@ -52,20 +45,6 @@ public class UserTest extends TestCase {
       exceptionThrown = true;
     }
     assertTrue(exceptionThrown);
-    Transaction.abort();
-  }
-  
-  public static boolean deleteDir(File dir) {
-    if (dir.isDirectory()) {
-      String[] children = dir.list();
-      for (int i=0; i<children.length; i++) {
-        boolean success = deleteDir(new File(dir, children[i]));
-        if (!success) {
-          return false;
-        }
-      }
-    }
-    return dir.delete();
   }
 
 }
