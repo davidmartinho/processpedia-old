988b5d685332c9e112d276502270d19a42bfd340
David Martinho <davidmartinho@gmail.com>
2011-07-19 16:06:46 +0100

    Changed the copyright header for all domain object class files

--- src/main/java/pt/ist/processpedia/domain/Queue.java
+++ src/main/java/pt/ist/processpedia/domain/Queue.java
@@ -1,6 +1,5 @@
-/**
- * Processpedia
- * Copyright (C) 2011 ESW Software Engineering Group
+/*
+ * Copyright 2011 ESW Software Engineering Group
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,7 +13,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- **/
+ */
 
 package pt.ist.processpedia.domain;
 
@@ -27,5 +26,5 @@ public class Queue extends Queue_Base {
   public Queue(String name) {
     setName(name);
   }
-    
+
 }
